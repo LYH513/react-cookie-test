5ff0b1a475e6ab220b78ef7185dddfd9f95f666c
LYH513 <peperony367@gmail.com>
2024-11-27 01:20:30 +0900

    test

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import KakaoMap from "../Components/Home_Components/KaKaoMap";
 
 //Home_Components의 컴포넌트들을 여기 불러오면 됨. 화면에 띄울 페이지
 function HomePage(){
 
   return(
-    <div>첫번째 화면</div>
+    <div>
+      <p>첫번째 화면</p>
+      <KakaoMap/>
+    </div>
   );
 }
 
